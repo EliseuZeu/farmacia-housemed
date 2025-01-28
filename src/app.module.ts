d04f4ab757cd7359630adc2f54d01dc9fae04092
EliseuZz <100594790+EliseuZeu@users.noreply.github.com>
2025-01-28 10:38:32 -0300

    configurando banco de dadoss

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({  //Configuração do banco de dados
+      type: 'mysql',  //Tipo do banco de dados
+      host: 'localhost',  //Endereço do banco de dados
+      port: 3306, //Porta do banco de dados
+      username: 'root', //Usuario do banco de dados
+      password: '193243', //Senha do banco de dados
+      database: 'db_housemed', //Banco de dados housemed farmacia 
+      entities: [],   //Entidades do banco de dados
+      synchronize: true, //Sincronização do banco de dados
+    })
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
